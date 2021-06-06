9377c915bb81767321b4d9f42da5580afc646e67
Keith Kang <keith.kang@kellogg.northwestern.edu>
2021-06-06 18:23:45 -0500

    index finished, bidding half finished

--- bid_plot.js
+++ bid_plot.js
@@ -1,55 +1,86 @@
-// Build the URL for bidding points API
-let url = `/.netlify/functions/course`
+window.addEventListener('DOMContentLoaded', async function() {
+  // Get a reference to the "go" button
+  let goButton = document.querySelector(`.go`)
 
-// Fetch the url, wait for a response, store the response in memory
-let response = await fetch(url)
+  // When the "go" button is clicked:
+  goButton.addEventListener(`click`, async function(event) {
+    // - Ignore the default behavior of the button
+    event.preventDefault()
 
-// Ask for the json-formatted data from the response, wait for the data, store it in memory
-let json = await response.json()
+    // - Get a reference to the element containing the course number
+    let numberInput = document.querySelector(`#courseNum`)
 
-// Write the json-formatted data to the console in Chrome
-console.log(json)
+    // - Get the user-entered location from the element's value
+    let courseNumber = numberInput.value
 
-// get a reference to the newly created post comment button
-let goButton = document.querySelector(`#courseName`)
+    // Build the URL for bidding points API
+    let url = `/.netlify/functions/bidding?courseNum=${courseNum}`
 
-// event listener for the go button
-goButton.addEventListener(`click`, async function(event) {
-  // ignore the default behavior
-  event.preventDefault()
+    // Fetch the url, wait for a response, store the response in memory
+    let response = await fetch(url)
 
-  // get a reference to the course name input
-  let courseNameInput = document.querySelector(`#courseName`)
+    // Ask for the json-formatted data from the response, wait for the data, store it in memory
+    let json = await response.json()
 
-  // get the body of the comment
-  let courseNameBody = courseNameInput.value
+    // Write the json-formatted data to the console in Chrome
+    console.log(json)
 
-  // Build the URL for our posts API
-  let url = `/.netlify/functions/create_comment?courseName=${courseNameBody}`
+    // Store course in memory 
+    let course = json
 
-  // Fetch the url, wait for a response, store the response in memory
-  let response = await fetch(url)
+    // Grab a reference to the element with class name "stats" in memory
+    let headerDiv = document.querySelector(`.stats`)
 
-  // refresh the page
-  location.reload()
+    // Create markup using the course data, insert into the "stats" element
+    headerDiv.innerHTML = `
+      <h2 class="font-bold">${course.number} - ${course.name}</h2>
+      <h3 class="font-bold">Historical average bidding points by phase</h3>
+      <div class='phase-stats flex'>
+      <d1  class = "w-1/3">Phase I: ${course.averagebidding1}</d1>
+      <d2  class = "w-1/3">Phase II: ${course.averagebidding2}</d2>
+      <d3  class = "w-1/3">Phase III: ${course.averagebidding3}</d3>
+    `
+
+    // Grab a reference to the element with class name "stats" in memory
+    let chartDiv = document.querySelector(`#myChart`)
+
+    // Create markup using the course data, insert into the "chart" element
+    chartDiv.innerHTML = `
+    <script>
+    // X value - quarter
+    var xValues = ["2019 Fall","2020 Winter","2020 Spring","2020 Fall","2021 Winter","2021 Spring"];
+
+    // Y value - bidding point by phase
+    new Chart("myChart", {
+      type: "line",
+      data: {
+      labels: xValues,
+      datasets: [{ 
+        data: [200,154,188,300,167,223],
+        borderColor: "red",
+        backgroundColor: "red",
+        fill: false,
+        label: "Phase I"  
+      }, { 
+        data: [658,700,659,578,800,737],
+        borderColor: "green",
+        backgroundColor: "green",
+        fill: false,
+        label: "Phase II"
+      }, { 
+        data: [300,267,344,146,100,400],
+        borderColor: "blue",
+        backgroundColor: "blue",
+        fill: false,
+        label: "Phase III"
+      }]
+    },
+      options: {
+        legend: {display: true, position: 'bottom'}
+      }
+    });
+    </script>
+
+    `
+  })
 })
-
-// Grab a reference to the element with class name "stats" in memory
-let headerDiv = document.querySelector(`.stats`)
-
-// Store course in memory 
-let course = json
-
-// Create markup using the course data, insert into the "stats" element
-headerDiv.innerHTML = `
-<h2 class="font-bold">${course.number} - ${course.name}</h2>
-<h3 class="font-bold">Historical average bidding points by phase</h3>
-<div class='phase-stats flex'>
-    <d1  class = "w-1/3">Phase I: 362</d1>
-    <d2  class = "w-1/3">Phase II: 812</d2>
-    <d3  class = "w-1/3">Phase III: 412</d3>
-`
-
-
-// Grab a reference to the element with class name "stats" in memory
-let chartDiv = document.querySelector(`#myChart`)
